5decc2bd41b2c794e70da8315df0204f6fbee57f
unknown <harshit.nitmz@gmail.com>
2023-07-31 04:45:09 +0530

    fix multiple selection of rating and final commit

--- script.js
+++ script.js
@@ -1,7 +1,40 @@
-const ratingBtnContainer=document.getElementById("select-rating")
+const ratingBtnContainer = document.getElementById("select-rating");
+const ratingBtns = document.querySelectorAll(".rating-btn");
+const submitBtn = document.getElementById("submit-btn");
+const thankYouCard = document.querySelector(".thank-you-card");
+const formCard = document.querySelector(".form-card");
+const ratingDetails = document.querySelector(".rating-details");
+const errorMessage=document.querySelector(".error-message")
 
-ratingBtnContainer.onclick=(e)=>{}
+let rating;
 
-const submitBtn=document.getElementById("submit-btn")
+thankYouCard.style.display = "none";
+errorMessage.style.display="none"
 
-submitBtn.onclick=(e)=>{}
+ratingBtnContainer.onclick=(e)=>{
+    e.preventDefault();
+    const btn=e.target;
+    rating=btn.value
+
+    // btn.classList.add('rating-btn-active') 
+    //adding in btn would result in a bug where if you click between the gaps of the buttons, that also changes add class as its inside btn container
+
+    ratingBtns.forEach((button)=>{
+        button!==btn?button.classList.remove("rating-btn-active"):button.classList.add("rating-btn-active")
+       
+    })
+
+}
+    
+
+submitBtn.onclick = (e) => {
+    if(rating)
+ { thankYouCard.style.display = "flex";
+  formCard.style.display = "none";
+  ratingDetails.textContent = `You selected ${rating} out of 5`;}
+  else{
+    errorMessage.style.display="inline"
+    errorMessage.textContent="Please enter the rating"
+  }
+ 
+};
